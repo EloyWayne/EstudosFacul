c5376c7a93086758b52e630291be7015e4f44a64
wayneEloy <138615880+EloyWayne@users.noreply.github.com>
2023-11-23 16:47:34 -0300

    Serializers/viewsets

--- EstudosApi/apps/produtos/api/viewsests.py
+++ EstudosApi/apps/produtos/api/viewsests.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .serializers import ProdutosSerializers
 from ..models import Produto
 
-class ProdutosSerializers(viewsets.ModelViewSet):
+class ProdutosViewsets(viewsets.ModelViewSet):
     serializer_class = ProdutosSerializers
     queryset = Produto.objects.all()
